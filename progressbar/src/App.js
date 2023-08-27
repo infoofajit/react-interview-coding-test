60de726a535930d86f582b240b2e2d63cd8c564d
infoofajit <infoofajit@gmail.com>
2023-08-27 21:12:44 +0530

    Add function to Start/Pause/Restart

--- progressbar/src/App.js
+++ progressbar/src/App.js
@@ -8,9 +8,20 @@ function App() {
   const [complete, setComplete] = useState(false)
 
   function start() {
+    setComplete(false)
+    pause()
     valInterval = setInterval(() => {
       setValue(val => val+1)
     }, 100)
+  }
+
+  function pause() {
+    clearInterval(valInterval)
+  }
+
+  function restart() {
+    setValue(0)
+    start()
   }
 
   useEffect(() => {
@@ -30,6 +41,13 @@ function App() {
           }}
         />
         <div>{complete ? 'Completed!': 'Loading...'}</div>
+      </div>
+      <div className="flex flex-row items-center">
+        <button onClick={() => {start()}} className="rounded-full px-4 py-1 bg-cyan-400">Start</button>
+        <button onClick={() => pause()} className="rounded-full px-4 py-1 bg-cyan-400 ml-2">
+          Pause
+        </button>
+        <button onClick={() => restart()} className="rounded-full px-4 py-1 bg-cyan-400 ml-2">Restart</button>
       </div>
     </div>
   )
